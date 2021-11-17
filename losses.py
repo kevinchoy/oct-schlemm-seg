e8b78695521dc65a7bbd1bcdb65b0a6200af25b3
kevinchoy <kevin.choy@duke.edu>
2021-11-17 16:42:13 -0500

    headers

--- losses.py
+++ losses.py
@@ -1,3 +1,11 @@
+"""Code for "K. C. Choy, G. Li, W. D. Stamer, S. Farsiu, Open-source deep learning-based automatic segmentation of
+mouse Schlemm’s canal in optical coherence tomography images. Experimental Eye Research, 108844 (2021)."
+Link: https://www.sciencedirect.com/science/article/pii/S0014483521004103
+DOI: 10.1016/j.exer.2021.108844
+The data and software here are only for research purposes. For licensing, please contact Duke University's Office of
+Licensing & Ventures (OLV). Please cite our corresponding paper if you use this material in any form. You may not
+redistribute our material without our written permission. """
+
 import torch
 import torch.nn.functional as F
 from torch import nn as nn
--- solver.py
+++ solver.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
+"""Code for "K. C. Choy, G. Li, W. D. Stamer, S. Farsiu, Open-source deep learning-based automatic segmentation of
+mouse Schlemm’s canal in optical coherence tomography images. Experimental Eye Research, 108844 (2021)."
+Link: https://www.sciencedirect.com/science/article/pii/S0014483521004103
+DOI: 10.1016/j.exer.2021.108844
+The data and software here are only for research purposes. For licensing, please contact Duke University's Office of
+Licensing & Ventures (OLV). Please cite our corresponding paper if you use this material in any form. You may not
+redistribute our material without our written permission. """
+
 import glob
 import os
 from pathlib import Path
